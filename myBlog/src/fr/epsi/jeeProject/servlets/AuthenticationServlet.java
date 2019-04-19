76df3f875f30c44dd6099af9a066486efa119e83
Laplijr <ronanlaplaud4@gmail.com>
2019-04-19 11:50:25 +0200

    create utilisateur servlet

--- myBlog/src/fr/epsi/jeeProject/servlets/AuthenticationServlet.java
+++ myBlog/src/fr/epsi/jeeProject/servlets/AuthenticationServlet.java
@@ -1,16 +1,20 @@
 package fr.epsi.jeeProject.servlets;
 
+import fr.epsi.jeeProject.beans.Utilisateur;
+import fr.epsi.jeeProject.dao.HSQLImpl.UtilisateurDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.rmi.CORBA.Util;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
-@WebServlet("/authentication")
+@WebServlet("/Authentication")
 public class AuthenticationServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LogManager.getLogger(TestServlet.class);
@@ -20,11 +24,19 @@ public class AuthenticationServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nom = request.getParameter("nom");
         request.getParameterMap().keySet().forEach(System.out::println);
 
-        logger.info("oui");
-        logger.info(nom);
+        Utilisateur user = new Utilisateur();
+        user.setAdmin(false);
+        user.setNom(request.getParameter("lastname") + ' ' + request.getParameter("firstname"));
+        user.setEmail(request.getParameter("email"));
+        user.setPassword(request.getParameter("password"));
+        try {
+            new UtilisateurDao().createUtilisateur(user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
