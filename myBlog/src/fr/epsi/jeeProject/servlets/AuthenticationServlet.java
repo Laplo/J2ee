7a42633f23bbeb1b0dd0fd55efa9a1a4471e2389
Laplijr <ronanlaplaud4@gmail.com>
2019-04-19 10:32:04 +0200

    add name paramaters

--- myBlog/src/fr/epsi/jeeProject/servlets/AuthenticationServlet.java
+++ myBlog/src/fr/epsi/jeeProject/servlets/AuthenticationServlet.java
@@ -1,5 +1,6 @@
 package fr.epsi.jeeProject.servlets;
 
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/AuthenticationServlet")
+@WebServlet("/Authentication")
 public class AuthenticationServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LogManager.getLogger(TestServlet.class);
@@ -21,12 +22,17 @@ public class AuthenticationServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String nom = request.getParameter("nom");
+        request.getParameterMap().keySet().forEach(param -> {
+            System.out.println(param);
+        });
+
         logger.info("oui");
         logger.info(nom);
-        this.getServletContext().getRequestDispatcher("index.jsp").forward(request, response);
+        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher("index.jsp").forward(request, response);
+
+        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
